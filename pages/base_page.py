10d3df3d3a05eed5b6c95e5dbe288cee80a37db7
GVanDerFlex <21missinglink21@gmail.com>
2020-09-07 10:29:34 +0300

    Added login link visibility test

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,18 @@
-from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage():
-    def __init__(self, browser : webdriver, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self, browser, url):
         self.browser.get(self.url)
+
+    def is_element_present(self, by, locator):
+        try:
+            self.browser.find_element(by, locator)
+        except NoSuchElementException:
+            return False
+        return True
