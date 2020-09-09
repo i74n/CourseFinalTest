6e7f2c05498f4797ee14d10295c4f235f9d921bb
GVanDerFlex <21missinglink21@gmail.com>
2020-09-09 03:28:05 +0300

    Login methods moved to BasePage class

--- pages/locators.py
+++ pages/locators.py
@@ -1,10 +1,6 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
-
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     SIGN_UP_FORM = (By.CSS_SELECTOR, "#register_form")
@@ -16,3 +12,8 @@ class ProductPageLocators():
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success strong")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
     SUCCESS_INFO = (By.CSS_SELECTOR, ".alert-info strong")
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- pages/product_page.py
+++ pages/product_page.py
@@ -12,3 +12,10 @@ class ProductPage(BasePage):
 
     def is_basket_total_equals_product_price(self):
         assert self.get_element_content(*ProductPageLocators.PRODUCT_PRICE) == self.get_element_content(*ProductPageLocators.SUCCESS_INFO)
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented"
+
+    def success_message_should_dissapear(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), "Success message not dissapeared"
+
