a9d0c673b709a28e406f55b2ff2d4a1fc3cb5a1c
GVanDerFlex <21missinglink21@gmail.com>
2020-09-10 17:18:42 +0300

    Added need_review mark

--- test_product_page.py
+++ test_product_page.py
@@ -23,6 +23,7 @@ class TestUserAddToBasketFromProductPage():
         product_page.open(browser, link)
         product_page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     @pytest.mark.parametrize('link',
                              ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
     def test_user_can_add_product_to_basket(self, browser, link):
@@ -60,6 +61,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     product_page.success_message_should_dissapear()
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
 def test_guest_can_add_product_to_basket(browser, link):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo={link}"
@@ -77,7 +79,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open(browser, link)
     page.should_be_login_link()
 
-
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -86,7 +88,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
-
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = ProductPage(browser, link)
